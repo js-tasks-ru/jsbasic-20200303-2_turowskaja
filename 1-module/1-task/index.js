c4bc098bf8d4f76c02d51503bc39257bbacb504d
turowskaja@gmail.com <turowskaja@gmail.com>
2020-03-10 16:45:51 +0300

    task 2

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,23 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
 
+function factorial(n) {
+  let result = 1;
+
+  if(n >= 0){
+    let i=1;
+    while (i<=n) {
+    result *= i;
+    i++;
+    }
+  } else {
+  result = false;
+  }
+  return result;
 }
+
+factorial(0);
+factorial(1);
+factorial(3);
+factorial(5);
