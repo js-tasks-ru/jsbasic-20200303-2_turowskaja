7c9f5e6469c259ff1b0359ebc41f657038a4521c
turowskaja@gmail.com <turowskaja@gmail.com>
2020-03-17 21:21:57 +0300

    task 3 module 3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -14,22 +14,15 @@
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 // window.calculator = calculator; // делает ваш калькулятор доступным глобально
 
-function sumInput(){
-  let sum = [];
-
-  while(true){
-    let userNumber = +prompt('Введите число', '');
-
-    if (typeof userNumber == "number" ) {
-      sum.push(userNumber);
-    } else{
-      break
-    }
+let calculator = {
+  read: function (num1, num2) {
+    this.a = num1;
+    this.b = num2;
+  },
+  sum: function () {
+    return this.a + this.b;
+  },
+  mul: function () {
+    return this.a * this.b;
   }
-  let numbers = 0;
-  for (let items of sum){
-    numbers += items;
-  }
-  return numbers;
-}
-alert(sumInput());
+};
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,9 +2,13 @@
  * @param {string} str
  * @returns {string}
  */
-function camelize(str) {
-	let arr = str.split('-').map(function (item){
-    return item[0].toUpperCase() + item.slice(1);
-	}).join('')
-  console.log(arr);
+function camelize(str){
+	let arr = str.split('-').map(function (item, index){
+    if(index == 0){
+      return item;
+    } else{
+     return item[0].toUpperCase() + item.slice(1); 
+    }
+  }).join('')
+  return arr;
 }
