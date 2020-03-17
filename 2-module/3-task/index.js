0940fb6269ab1b1e9ff105f06fed48e63c8dc38b
turowskaja@gmail.com <turowskaja@gmail.com>
2020-03-17 18:37:31 +0300

    task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,15 +1,35 @@
-let calculator = {
-  read: function (num1, num2) {
-    this.a = num1;
-    this.b = num2;
-  },
-  sum: function () {
-    return this.a + this.b;
-  },
-  mul: function () {
-    return this.a * this.b;
-  }
-};
+//let calculator = {
+//   read: function (num1, num2) {
+//     this.a = num1;
+//     this.b = num2;
+//   },
+//   sum: function () {
+//     return this.a + this.b;
+//   },
+//   mul: function () {
+//     return this.a * this.b;
+//   }
+// };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-window.calculator = calculator; // делает ваш калькулятор доступным глобально
+// window.calculator = calculator; // делает ваш калькулятор доступным глобально
+
+function sumInput(){
+  let sum = [];
+
+  while(true){
+    let userNumber = +prompt('Введите число', '');
+
+    if (typeof userNumber == "number" ) {
+      sum.push(userNumber);
+    } else{
+      break
+    }
+  }
+  let numbers = 0;
+  for (let items of sum){
+    numbers += items;
+  }
+  return numbers;
+}
+alert(sumInput());
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,8 @@
  * @returns {string}
  */
 function camelize(str) {
+	let arr = str.split('-').map(function (item){
+    return item[0].toUpperCase() + item.slice(1);
+	}).join('')
+  console.log(arr);
 }
