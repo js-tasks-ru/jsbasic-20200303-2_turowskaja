7dc4b8b1db9998b9fbd550eb1d6f61e178d6a97e
turowskaja@gmail.com <turowskaja@gmail.com>
2020-03-23 22:54:42 +0300

    task 1 module 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,13 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+    let arr = friends.map(function(item){
+        return item.firstName + " " + item.lastName;
+      });
+    
+    let ul = document.createElement('ul');
+    ul.innerHTML = `<li> ${arr[0]} </li> <li> ${arr[1]} </li> <li> ${arr[2]} </li>`;
+    document.body.append(ul);
+    
+    return document.querySelector('ul');
 }
